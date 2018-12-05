9128b223041e9027aba88281c06efe163407684d
nvvssmanyam <nvvss009@gmail.com>
2018-12-05 14:34:12 +0530

    Added CORS

--- DepartmentalStores/DepartmentalStores/settings.py
+++ DepartmentalStores/DepartmentalStores/settings.py
@@ -38,19 +38,27 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     # Activating user defined models
     'stores.apps.LocationsConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    '*:8000',
+)
 
 ROOT_URLCONF = 'DepartmentalStores.urls'
 
